2acba50bda74a53386fb82e0241cccf0a34ea5c8
Sophie Bredenkamp <sabredenkamp@gmail.com>
2022-03-05 12:08:25 -0700

    Initial Commit

---
+++ TriTraining/TriTrainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriTrainingApp.swift
+//  TriTraining
+//
+//  Created by Sophie Bredenkamp on 3/5/22.
+//
+
+import SwiftUI
+
+@main
+struct TriTrainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
